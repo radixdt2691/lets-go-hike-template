548faadf90fb28558437740e24b1d22c9d8733d5
radixdt2691 <radixdt.2691@gmail.com>
2023-05-18 12:05:47 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("navbar");
 var nav = document.getElementById("myTopnav");
 var logo = document.getElementById("main-logo");
+var links = document.querySelectorAll("#nav-links");
 var togglebtn = document.getElementById("btn-nav");
 var sticky = header.offsetTop;
 
@@ -16,9 +16,15 @@ if (window.pageYOffset > sticky) {
     header.classList.add("sticky","top-0","shadow");
     nav.classList.add("justify-start","p-10");
     logo.classList.add("img-fluid");
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.add('font-25');
+    }
 } else {
     header.classList.remove("sticky","top-0","shadow");
     nav.classList.remove("justify-start","p-10");
     logo.classList.remove("img-fluid");
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.remove('font-25');
+    }
 }
 }
