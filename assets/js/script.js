1b51f847466625b597e6634467b94126d4649c29
radixdt2691 <radixdt.2691@gmail.com>
2023-05-19 12:19:31 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,14 @@ function changeIcon(){
 }
 function myFunction() {
 if (window.pageYOffset > sticky) {
-    header.classList.add("sticky","top-0","shadow");
+    header.classList.add("sticky","top-0","shadow","fixed-border");
     nav.classList.add("justify-start","p-10");
     logo.classList.add("img-fluid");
     for (var i = 0; i < links.length; i++) {
         links[i].classList.add('font-25');
     }
 } else {
-    header.classList.remove("sticky","top-0","shadow");
+    header.classList.remove("sticky","top-0","shadow","fixed-border");
     nav.classList.remove("justify-start","p-10");
     logo.classList.remove("img-fluid");
     for (var i = 0; i < links.length; i++) {
